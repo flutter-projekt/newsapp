6998ae533f743dd6dada597dc79f2ee91900aa48
franzi1999 <61967851+franzi1999@users.noreply.github.com>
2023-04-28 13:14:01 +0200

    Faavoriten vorbereitet

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -9,8 +9,11 @@ class FavoritesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        shadowColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        centerTitle: true,
         title: const Text(
-          'Profil',
+          'Favorites',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
@@ -19,83 +22,10 @@ class FavoritesScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.transparent,
         elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              // TODO: navigate to settings screen
-            },
-          ),
-        ],
+        actions: [],
       ),
       body: Column(
-        children: [
-          SizedBox(height: 20),
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: NetworkImage(
-              'https://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-hamsters-1__880.jpg',
-            ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Hamilton',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'hamsterhamilton@gmail.com',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey[600],
-            ),
-          ),
-          SizedBox(height: 20),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text('Favoriten'),
-            trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () {
-              // TODO: navigate to favorites screen
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.history),
-            title: Text('Verlauf'),
-            trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () {
-              // TODO: navigate to history screen
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.notifications),
-            title: Text('Benachrichtigungen'),
-            trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () {
-              // TODO: navigate to notifications screen
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.share),
-            title: Text('Teilen'),
-            trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () {
-              // TODO: implement share functionality
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.rate_review),
-            title: Text('Bewerte unsere App'),
-            trailing: Icon(Icons.arrow_forward_ios),
-            onTap: () {
-              // TODO: navigate to app rating screen
-            },
-          ),
-        ],
+        children: [],
       ),
       bottomNavigationBar: const BottomNavBar(index: 2),
     );
